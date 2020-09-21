06cf9580515318c2ea81507d6b72909b367e5453
Aron Schiphof <aron.schiphof@gmail.com>
2020-09-21 11:11:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/madlevel2task1/PlaceAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.madlevel2task1
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel2task1.databinding.ItemPlaceBinding
+
+public class PlaceAdapter(private val places: List<Place>) :
+    RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView : View) :
+        RecyclerView.ViewHolder(itemView) {
+
+        val binding = ItemPlaceBinding.bind(itemView)
+
+        fun bind(place : Place) {
+            binding.ivPlace.setImageResource(place.imageResId)
+            binding.tvPlace.text = place.name
+        }
+    }
+
+    /**
+     * Creates and returns a ViewHolder object, inflating a standard layout called simple_list_item_1.
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_place, parent, false)
+        )
+    }
+
+    /**
+     * Returns the size of the list
+     */
+    override fun getItemCount(): Int {
+        return places.size
+    }
+
+    /**
+     * Called by RecyclerView to display the data at the specified position.
+     */
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(places[position])
+    }
+}
